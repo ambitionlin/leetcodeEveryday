8f018923c145952647369912051238a3d21a41bb
Wan Junlin <33538213+ambitionlin@users.noreply.github.com>
2020-01-22 21:08:57 +0800

    commit topic494_目标和

---
+++ topic494_目标和/DFS.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int S) {
+       return findTargetSumWays(nums,0,S);
+    }
+    public int findTargetSumWays(int[] nums, int start, int S){
+        if(start==nums.length){
+            return S==0?1:0;
+        }
+        return findTargetSumWays(nums,start+1,S+nums[start])+findTargetSumWays(nums,start+1,S-nums[start]);
+    }
+}
