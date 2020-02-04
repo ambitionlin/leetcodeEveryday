75835203204e5e4903c94b4b6bd0605c6247965d
Wan Junlin <33538213+ambitionlin@users.noreply.github.com>
2020-02-04 11:47:34 +0800

    commit  topic200_岛屿数量

--- topic200_岛屿数量/BFS.java
+++ topic200_岛屿数量/BFS.java
@@ -1,5 +1,37 @@
 class Solution {
-    public int openLock(String[] deadends, String target) {
-
+    private int[][] Directions = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+    private int m,n;
+    public  int numIslands(char[][] grid){
+        Queue<int[]> queue = new LinkedList<>();
+        m = grid.length;
+        if(m==0){
+            return 0;
+        }
+        n = grid[0].length;
+        int level = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1'){
+                    level++;
+                    grid[i][j]='0';
+                    queue.add(new int[]{i,j});
+                    while (!queue.isEmpty()){
+                        int[] point = queue.poll();
+                        int row = point[0];
+                        int col = point[1];
+                        for(int[] d:Directions){
+                            int r = row+d[0];
+                            int c = col+d[1];
+                            if(r<0||r>=m||c<0||c>=n||grid[r][c]=='0'){
+                                continue;
+                            }
+                            grid[r][c]='0';
+                            queue.add(new int[]{r,c});
+                        }
+                    }
+                }
+            }
+        }
+        return level;
     }
 }
