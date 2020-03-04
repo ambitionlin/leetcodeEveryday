6f527c8627b70792685a7693a012681c1f86544e
Wan Junlin <33538213+ambitionlin@users.noreply.github.com>
2020-03-04 21:14:46 +0800

    commit topic151_翻转字符串里的单词

---
+++ topic151_翻转字符串里的单词/StringBuilder.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String reverseWords(String s) {
+        if(s==null||s.length()==0){
+            return s;
+        }
+        String[] words = s.trim().split(" +");
+        StringBuilder sb = new StringBuilder();
+        for(int i=words.length-1;i>=0;i--){
+            sb.append(words[i]+" ");
+        }
+        return sb.toString().trim();  
+    }
+}
