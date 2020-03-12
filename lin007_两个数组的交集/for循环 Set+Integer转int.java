fb6c00d65cde5ff74c4a879234c2f008d6248077
Wan Junlin <33538213+ambitionlin@users.noreply.github.com>
2020-03-12 22:51:45 +0800

    commit lin007_两个数组的交集

---
+++ lin007_两个数组的交集/for循环 Set+Integer转int.java
@@ -0,0 +1,24 @@
+
+import java.util.HashSet;
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer>  set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        for(int i=0;i<nums1.length;i++){
+            set1.add(nums1[i]);
+        }
+        for(int j=0;j<nums2.length;j++){
+            if(set1.contains(nums2[j])){
+                set2.add(nums2[j]);
+            }
+        }
+
+        Integer[] intRes = set2.toArray(new Integer[]{});
+        int[] result = new int[intRes.length];
+        for(int i=0;i<intRes.length;i++){
+            result[i] = intRes[i].intValue();
+        }
+        return result;
+    }
+}
